0474e7bed35d52de54c2be117e87ded29e475678
Mateusfm <mateusfm95@gamil.com>
2016-10-11 20:32:28 -0300

    login completo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,23 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_no_authentication, only: [:new, :create]
+  before_action :can_change, only: [:edit, :update]
+  
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
  	@user = User.find(params[:id])
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -39,9 +33,8 @@ class UsersController < ApplicationController
       end
     end
   end
+ 
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -54,8 +47,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+
   def destroy
     @user.destroy
     respond_to do |format|
@@ -64,14 +56,42 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def can_change
+       unless user_signed_in? && current_user == user
+         redirect_to user_path(params[:id])
+       end
+    end
+
+   
+    def user
+      @user ||= User.find(params[:id])
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
