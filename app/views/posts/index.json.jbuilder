e48dd036e165011f55d33b099979a516bf516cab
Mateusfm <mateusfm95@gamil.com>
2016-10-22 16:24:37 -0200

    permissao para o modelo post

--- app/views/posts/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@posts) do |post|
-  json.extract! post, :id, :titulo, :subtitulo, :texto
-  json.url post_url(post, format: :json)
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017175516) do
+ActiveRecord::Schema.define(version: 20161022165802) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -23,8 +23,12 @@ ActiveRecord::Schema.define(version: 20161017175516) do
     t.string   "titulo"
     t.string   "subtitulo"
     t.text     "texto"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "produtos", force: :cascade do |t|
